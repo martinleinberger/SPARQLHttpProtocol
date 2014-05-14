62d9c3a51f6971f0ca4de1ce635c5f86e030bc48
Martin Leinberger <martin.leinberger@googlemail.com>
2014-05-14 18:03:49 +0200

    Testing code

--- SPARQLHttpProtocol/Script.fsx
+++ SPARQLHttpProtocol/Script.fsx
@@ -1,8 +1,10 @@
 ﻿// Weitere Informationen zu F# finden Sie unter http://fsharp.net. Im Projekt 'F#-Lernprogramm' finden Sie
 // einen Leitfaden zum Programmieren in F#.
 
-#load "Library1.fs"
+#r "../packages/FSharp.Data.2.0.8/lib/net40/FSharp.Data.dll"
+#load "SPARQLHttpEndpoint.fs"
 open SPARQLHttpProtocol
 
 // Skriptcode für die Bibliothek hier definieren
-
+let endpoint = new Endpoint("http://stardog.west.uni-koblenz.de:8080/openrdf-sesame/repositories/test", "")
+endpoint.Query("SELECT DISTINCT * WHERE { ?s ?p ?o .}") |> printfn "%A"
